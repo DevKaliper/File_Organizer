ac96b33fe2924a96799a9bbc6cc79e81f817e6b0
Dev Kaliper <gxlthz@gmail.com>
2023-07-08 16:38:17 -0500

    script terminado, probando

--- file_organizer.py
+++ file_organizer.py
@@ -1,6 +1,46 @@
 import os
 import shutil
 
+EXTENSIONS = {
+    "Fotos": [".jpg", ".png", ".jpeg"],
+    "Videos":[".mp4", ".mov", ".avi", ".wav"],
+    "Audio":[".mp3"],
+    "PDF": [".pdf"],
+    "Word":[".docx", ".dotx"],
+    "Programas":[".exe"]
+
+}
+
+def get_extentions(path):
+    extentions = []
+    for iteration in os.listdir(path):
+        ext = os.path.splitext(iteration)[1]
+        if ext not in extentions and ext != "":
+            extentions.append(ext)
+    
+    return extentions
+
+
+
+def create_dirs(path):
+    extentions = get_extentions(path)
+    for dir in EXTENSIONS.keys():
+        for file in extentions:
+            if file in EXTENSIONS[dir] and not os.path.exists(path+dir):
+                os.mkdir(path+dir)
+                
+        
+
+def move_files(path, file, ext):
+    for dir in EXTENSIONS.keys():
+        if ext in EXTENSIONS[dir]:
+            try:
+                shutil.move(path+file, path+dir)
+            except:
+                print(f"Esta ruta: {path+file}  dio error al intentar moverla.")
+
+
+
 while True:
     path = input("Dame la ruta la cual quieres organizar: ")
     if os.path.exists(path):
@@ -10,7 +50,18 @@ while True:
     print("Ruta no encontrada...")
 
 
+    
+def organizer(path):
+    create_dirs(path)
+    for file in os.listdir(path):
+        ext = os.path.splitext(file)[1]
+        move_files(path, file, ext)
 
+
+
+
+
+organizer(path)
 print(f"proceso terminado. la ruta es {path} ")
-    
-    
+
+
