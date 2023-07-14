d823a04f0eff124c38a28da50c0d222d170b7cd7
Dev Kaliper <gxlthz@gmail.com>
2023-07-14 00:29:09 -0500

    Haciendo pruebas

--- windowss.py
+++ windowss.py
@@ -1,11 +1,27 @@
 from tkinter import *
 from tkinter import ttk
+import os
+
+
+def pathFinder(path):
+    if os.path.exists(path):
+        print(f"Ruta encontrada: {path} ")
+    else:
+        print(f"No he podido encontrar la ruta: {path}")
+
+
 root = Tk()
 root.title("Probando ttk")
+
+
 root.geometry("300x200")
 entry = ttk.Entry()
-entry.insert(0, "Hola test")
+
 entry.place(x=50, y=50)
-button = ttk.Button(text="Obtener texto", command=lambda: print(entry.get()))
+label = ttk.Label(text="Hola mundo")
+label.place(x=50, y=150)
+
+button = ttk.Button(text="Coloca la ruta a organizar",
+                    command=lambda: pathFinder(entry.get()))
 button.place(x=50, y=100)
 root.mainloop()
